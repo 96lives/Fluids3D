813c268868dd2762e611fb9f2915263e2557f469
ds <96lives@gmail.com>
2020-06-20 13:55:47 +0900

    add report and video

---
+++ code/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/FluidRenderer3d.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/FluidRenderer3d.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/FluidSim3dMain.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/FluidSim3dMain.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/FluidSolver3d.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/FluidSolver3d.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/MarchingCubes.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/MarchingCubes.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/RenderUtil.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/RenderUtil.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/SimUtil.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/SimUtil.cpp.o"
+  "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/classicSolver.cpp" "/home/ds/projects/fluid-sim-3d/FluidSim3D/src/CMakeFiles/main.dir/classicSolver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/src/cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../main"
+  "../main.pdb"
+  "CMakeFiles/main.dir/FluidRenderer3d.cpp.o"
+  "CMakeFiles/main.dir/FluidSim3dMain.cpp.o"
+  "CMakeFiles/main.dir/FluidSolver3d.cpp.o"
+  "CMakeFiles/main.dir/MarchingCubes.cpp.o"
+  "CMakeFiles/main.dir/RenderUtil.cpp.o"
+  "CMakeFiles/main.dir/SimUtil.cpp.o"
+  "CMakeFiles/main.dir/classicSolver.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
